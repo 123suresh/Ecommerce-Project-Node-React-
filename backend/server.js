842d863dd1885c5390d1b6ebd8a4e98f2641e98a
sureshthapa <suresh.thapa@berrybytes.com>
2022-09-11 19:35:27 +0545

    search filter and pagination added

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,13 @@ const connectDatabse = require('./config/database');
 
 const dotenv = require('dotenv');
 
+//Handle uncaught exceptions
+process.on('uncaughtException', err => {
+    console.log(`ERROR: ${err.message}`);
+    console.log('Shutting down due to uncaught exception');
+    process.exit(1);
+})
+
 //setting up config file
 dotenv.config({path:'backend/config/config.env'})
 
@@ -10,6 +17,15 @@ dotenv.config({path:'backend/config/config.env'})
 connectDatabse();
 
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
+})
+
+//Handle unhandled promise rejection
+process.on('unhandledRejection', err => {
+    console.log(`ERROR: ${err.message}`);
+    console.log("Shutting down server due to unhandle promise rejection");
+    server.close(() => {
+        process.exit(1);
+    })
 })
